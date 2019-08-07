474f88ece0b8f8b02a8842c8c1a3a1ff8ccba69e
unknown <sdesigncba@gmail.com>
2019-08-07 14:29:06 -0300

    ajustes nueva web

--- nueva-web/form.php
+++ nueva-web/form.php
@@ -1,41 +1,133 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') { //solo ingreso a este bloque de código si el método con el que solicita la página es POST
-    $tiempoEspera = 3; //tiempo de espera para recargar la página (aplicado en la lógica de refresh)
+require 'PHPMailerAutoload.php';
+date_default_timezone_set('America/Argentina/Buenos_Aires');
+// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
+//$email_to = "sdesigncba@gmail.com";
+//$email_to = "carlosdanielgutierrez@gmail.com";
 
-    $origenNombre = 'Nueva cliente HABLA'; //nombre que visualiza el receptor del email como "origen" del email (es quien envía el email)
-    $origenEmail = 'noreply@habla.com';//email que visualiza el receptor del email como "origen" del email (es quien envía el email)
-    $destinatarioEmail = 'sprados@chimpancedigital.com.ar'.','.'quien@habla.com.ar'; //destinatario del email, o sea, a quien le estamos enviando el email
 
-    $nombre = $_POST['nombre'];
-    $dni = $_POST['dni'];
-    $email = $_POST['email'];
-    $empresa = $_POST['empresa'];
+// $email_to ="info@ralseff.com";
 
-    
-    $asuntoEmail = 'Nuevo cliente'; //asunto del email
-    
-    //cuerpo del email:
-    $cuerpoMensaje = "Nombre: ".$nombre."\r\n";
-    $cuerpoMensaje .= "DNI: ".$dni."\r\n";
-    $cuerpoMensaje .= "Empresa: ".$empresa."\r\n";
-    $cuerpoMensaje .= "Email: ".$email;
-    //fin cuerpo del email.
-    
-    //cabecera del email (forma correcta de codificarla)
-    $header = "From: " . $origenNombre . " <" . $origenEmail . ">\r\n";
-    $header .= "Reply-To: " . $origenEmail . "\r\n";
-    $header .= "MIME-Version: 1.0\r\n";
-    // $header .= "Content-Type: multipart/mixed; boundary=\"" . $uid . "\"\r\n\r\n";
-    //armado del mensaje y attachment
-    $mensaje .= $cuerpoMensaje . "\r\n\r\n";
-    //envio el email y verifico la respuesta de la función "email" (true o false)
-    if (mail($destinatarioEmail, $asuntoEmail, $mensaje, $header)) {
-        echo 'Mensaje enviado!';
-    } else {
-        echo 'Error, no se pudo enviar el email';
-    }
-    // echo ', la página será recargada en ' . $tiempoEspera . ' segundos.';
-    // echo '<meta http-equiv="refresh" content="' . $tiempoEspera . '">';
-    exit();
+$nombre = $_POST['nombre'];
+$dni = $_POST['dni'];
+$email = $_POST['email'];
+$empresa = $_POST['empresa'];
+
+
+$email_subject = "Consulta campaña Clínica de ojos";
+$email_subject2 = "Confirmación de trabajo Habla!";
+
+// Aquí se deberían validar los datos ingresados por el usuario
+if(!isset($_POST['nombre']) ||
+!isset($_POST['dni']) ||
+!isset($_POST['email']) ||
+!isset($_POST['empresa'])) {
+
+echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
+echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
+die();
 }
+
+$email_message = "Confirmación de terminos y condiciones :\n\n";
+$email_message .= "Este mail confirma que " . $_POST['name'] . "Acepta los terminos y condiciones de trabajo estipulados por Habla! \n";
+$email_message .= "Gracias por elegirnos \n";
+$email_message .= "Habla!";
+$email_message2 = "<h1>Detalles del formulario :</h1><br>";
+$email_message2 .= "<p>Nombre y Apellido: " . $_POST['nombre'] ."</p>";
+$email_message2 .= "<p>Mail: " . $_POST['email'] ."</p>";
+$email_message2 .= "<p>DNI: " . $_POST['dni'] ."</p>";
+$email_message2 .= "<p>Empresa: " . $_POST['empresa'] ."</p>";
+
+
+//inicio script grabar datos en csv
+$fichero = 'terminos y condiciones aceptados.csv';//nombre archivo ya creado
+//crear linea de datos separado por coma
+$fecha=date("d-m-y H:i:s");
+$linea = $fecha.";".$name.";".$email.";".$dni.";".$empresa."\n";
+// Escribir la linea en el fichero
+file_put_contents($fichero, $linea, FILE_APPEND | LOCK_EX);
+//fin grabar datos
+// $message=$message.' local='.$local;
+// $mail = new PHPMailer;
+// $mail->isSMTP();
+
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->SMTPDebug = 0;
+$mail->Debugoutput = 'html';
+
+$mail->Host = 'silex14web.com';
+$mail->Port = 2525;
+$mail->SMTPAuth = true;
+$mail->SMTPOptions = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+    )
+);
+// $mail->SMTPSecure = false;
+// $mail->SMTPAutoTLS = false;
+// $mail->SMTPOptions = array(
+//     'ssl' => array(
+//         'verify_peer' => false,
+//         'verify_peer_name' => false,
+//         'allow_self_signed' => true
+//     )
+// );
+$mail->Username = 'quien-habla.com.ar';
+$mail->Password = 'Habla753';
+$mail->setFrom('quien@habla.com.ar', 'Habla');
+
+$mail->addReplyTo('quien@habla.com.ar','Habla');
+
+$mail->addAddress('quien@habla.com.ar','Habla');
+// $mail->addCc('cristiancastro.pr1991@gmail.com','Clínica Santia Lucia');
+// $mail->addCc('quirofanosantaluciasalta@gmail.com','Clínica Santia Lucia');
+$mail->isHTML(true);
+$mail->Subject = $email_subject;
+$mail->Body    = $email_message2;
+$mail->Send();
+
+$mail->ClearAddresses();
+
+$mail->addAddress ($email);
+$mail->Body    = $email_message;
+$mail->Subject = $email_subject2;
+$mail->Send();
+
+
+$mail->CharSet = 'UTF-8';
+if (!$mail->send()) {
+    $mail_enviado=false;
+    $mail_error .= 'Mailer Error: '.$mail->ErrorInfo;
+} else {
+    $mail_enviado=true;
+    $mail_error='Mensaje Enviado, Gracias';
+}
+// Ahora se envía el e-mail usando la función mail() de PHP
+//$headers = 'From: Ralseff <info@ralseff.com>' . "\r\n" .
+//    'Reply-To: noreply@ralseff.com' . "\r\n" .
+//    'Cc: ralseff@chimpancedigital.com.ar' . "\r\n" .
+//    'X-Mailer: PHP/' . phpversion();
+//$mail_enviado = @mail($email_to, utf8_decode($email_subject), utf8_decode($email_message), $headers);
+
+
+if($mail_enviado)
+{
+echo "<script>location.href='gracias.html';</script>";
+
+}
+else
+{
+	echo "no se pudo enviar" ;
+}
+
+// Envia un e-mail para el remitente, agradeciendo la visita en el sitio, y diciendo que en breve el e-mail sera respondido. 
+// $mensaje2  = "Hola" . $_POST['name'] . ". Gracias por contactarnos. Un asesor se comunicará con usted a la brevedad..."; 
+// $mensaje2 .= "PD - No es necesario responder este mensaje."; 
+// $envia =  mail($_POST['email'],"Su mensaje fué recibido!",$mensaje2,$headers);
+
+
+
 ?>
